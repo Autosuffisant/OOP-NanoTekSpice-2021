6532f42d633d75fa1018ab97778e1d310e99f295
Prism <63168542+Autosuffisant@users.noreply.github.com>
2021-05-29 12:44:57 +0200

    Create GetFileContent.hpp

---
+++ include/GetFileContent.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2021
+** GetFileContent
+** File description:
+** returns content of a file
+*/
+
+#ifndef MY_GETFILECONTENT_
+#define MY_GETFILECONTENT_
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+
+std::vector<std::string> getFileContent(std::string fileName);
+
+#endif /* !MY_GETFILECONTENT_ */
